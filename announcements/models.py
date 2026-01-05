2539d1e41fabccc518b2c0c774aac7ea9474cbb9
vennie0311 <maryven.serato@evsu.edu.ph>
2026-01-05 22:59:00 +0800

    Assignment 4: comments forms

--- announcements/models.py
+++ announcements/models.py
@@ -7,3 +7,17 @@ class Announcement(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    announcement = models.ForeignKey(
+        Announcement,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.announcement.title}"
--- announcements/views.py
+++ announcements/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Announcement
+from .forms import CommentForm
+
 
 def home(request):
     return render(request, 'home.html')
+
 
 def announcement_list(request):
     qs = Announcement.objects.order_by('-created_at')
     return render(request, 'announcements/list.html', {'announcements': qs})
 
+
 def announcement_detail(request, id):
-    ann = get_object_or_404(Announcement, pk=id)
-    return render(request, 'announcements/detail.html', {'announcement': ann})
+    announcement = get_object_or_404(Announcement, pk=id)
+    comments = announcement.comments.all().order_by('-created_at')
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.announcement = announcement
+            comment.save()
+            return redirect("announcement_detail", id=announcement.id)
+    else:
+        form = CommentForm()
+
+    return render(
+        request,
+        "announcements/detail.html",
+        {
+            "announcement": announcement,
+            "comments": comments,
+            "form": form,
+        }
+    )
